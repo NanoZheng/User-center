e7082149166ade0b2eb2bfa3f58ceb18e494cbaf
z5355074 <z5355074@ad.unsw.edu.au>
2023-09-09 15:00:15 +1000

    initiate

---
+++ backend/src/main/java/com/nano/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.nano.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nano.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
